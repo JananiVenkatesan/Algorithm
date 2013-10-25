c2225e805fad5a7d22457f4d934dfd42d1db813e
phoenixjiangnan <bowenli86@gmail.com>
2013-10-25 14:43:11 -0400

    .

---
+++ Cracking the Coding Interview/chapter 9/C9Q5.java
@@ -0,0 +1,37 @@
+/*
+Problem:
+	Write a method to compute all permutations of a string
+
+Solution:
+	page 324
+
+	This solution takes 0(n!) time, since there are n! permutations.
+We cannot do better than this.
+*/
+
+public static ArrayList<String> getPerms(String str){
+	if(str == null){  // special case
+		return null;
+	}
+	ArrayList<String> permutations = new ArrayList<String>();
+	if(str.length() == 0){  // base case
+		permutations.add("");
+		return permutations;
+	}
+	char first = str.char(0);  // get the first character
+	String remainder = str.substring(1);  // remove the 1st character
+	ArrayList<String> words = getPerms(remainder);
+	for(String word : words){
+		for(int i = 0; i <= word.length(); i++){
+			String s = insertCharAt(word, first, i);
+			permutations.add(s);
+		}
+	}
+	return permutations;
+}
+
+public static String insertCharAt(String word, char ch, int index){
+	String first = word.substring(0, index);
+	String second = word.substring(index);
+	return first + ch + second;
+}
