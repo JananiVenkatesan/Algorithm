b28b051c147465a0dff64a37149d22e5e9e2cceb
phoenixjiangnan <bowenli86@gmail.com>
2013-11-25 00:37:09 -0500

    .

--- chapter 9 Recurse and Dynamic Programming/C9Q8 Count Cents.java
+++ chapter 9 Recurse and Dynamic Programming/C9Q8 Count Cents.java
@@ -10,10 +10,41 @@ calculate the number of ways of representing n cents.
 
 */
 
-public int makeChange(int n){
-	if(n < 0)
-		return 0;
-	if(n == 0)
-		return 1;
+import java.util.*;
 
+class Solution{
+    public int makeChange(int n){
+        if(n < 0)
+            return 0;
+        return makeChange(n, 25);
+    }
+
+    public int makeChange(int n, int denom){
+	    int next_denom = 0;
+	    switch(denom){
+	        case 25 :
+	            next_denom = 10;
+	            break;
+	        case 10 :
+	            next_denom = 5;
+	            break;
+	        case 5 :
+	            next_denom = 1;
+	            break;
+	        case 1 :
+	            return 1;
+	    }
+	    int ways = 0;
+	    for(int i = 0; i * denom <= n; i ++){
+	        ways += makeChange(n - i * denom, next_denom);
+	    }
+	    return ways;
+	}
+}
+
+public class Leetcode{
+    public static void main(String[] args){
+        Solution s = new Solution();
+        System.out.println(s.makeChange(598));
+    }
 }
